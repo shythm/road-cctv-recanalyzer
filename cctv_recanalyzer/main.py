8a374f0bbc28671a7e01af8b8bfee75e6be82ab2
Seongho Lee <shythm@outlook.com>
2024-08-18 02:33:29 +0900

    feat: add yolov8+deepsort tracking task service at main

--- cctv_recanalyzer/main.py
+++ cctv_recanalyzer/main.py
@@ -1,8 +1,9 @@
 import os
 
 from util import get_env_force
-from fastapi import FastAPI, HTTPException, Request, responses
-from datetime import datetime
+from fastapi import FastAPI, APIRouter, Request, Depends, Query, responses
+from typing import Optional, Type
+from pydantic import create_model, BaseModel, Field
 
 from core.model import CCTVStream, TaskItem, TaskOutput, EntityNotFound
 from core.repo import CCTVStreamRepository, TaskItemRepository, TaskOutputRepository
@@ -12,6 +13,7 @@ from repo.cctv_stream_its import CCTVStreamITSRepo
 from repo.task_output_file import TaskOutputFileRepo
 from repo.task_item_file import TaskItemJsonRepo
 from srv.cctv_record_ffmpeg import CCTVRecordFFmpegTaskSrv
+from srv.cctv_yolov8_deepsort import YOLOv8DeepSORTTackingTaskSrv
 
 JSON_DB_STORAGE = get_env_force('JSON_DB_STORAGE')
 ITS_API_KEY = get_env_force('ITS_API_KEY')
@@ -34,6 +36,46 @@ cctv_record_srv: TaskService = CCTVRecordFFmpegTaskSrv(
     outputs_path=TASK_OUTPUT_PATH,
     output_repo=task_output_repo,
 )
+cctv_tracking_srv: TaskService = YOLOv8DeepSORTTackingTaskSrv(
+    task_repo=task_item_repo,
+    model_path='yolov8l.pt',
+    outputs_path=TASK_OUTPUT_PATH,
+    output_repo=task_output_repo,
+)
+
+def create_task_router(task_service: TaskService) -> APIRouter:
+    def read_task_list() -> list[TaskItem]:
+        return task_service.get_tasks()
+
+    start_query_params = {}
+    for param in task_service.get_params():
+        start_query_params[param.name] = (
+            Optional[str] if param.optional else str,
+            Field(Query(None if param.optional else ..., description=param.desc)),
+        )
+    StartQueryModel: Type[BaseModel] = create_model('StartTaskParams', **start_query_params)
+
+    def start_task(params: StartQueryModel=Depends()) -> TaskItem: # type: ignore
+        # remove None values
+        params = params.dict()
+        params = { k: v for k, v in params.items() if v is not None }
+        return task_service.start(params=params)
+
+    def stop_task(taskid: str):
+        return task_service.stop(taskid)
+
+    def delete_task(taskid: str):
+        return task_service.del_task(taskid)
+
+    router = APIRouter()
+    tags = [task_service.get_name()]
+
+    router.add_api_route("", read_task_list, methods=['GET'], tags=tags)
+    router.add_api_route("/start", start_task, methods=['POST'], tags=tags)
+    router.add_api_route("/stop/{taskid}", stop_task, methods=['POST'], tags=tags)
+    router.add_api_route("/{taskid}", delete_task, methods=['DELETE'], tags=tags)
+
+    return router
 
 app = FastAPI()
 
@@ -79,21 +121,8 @@ def create_cctv_stream(cctvname: str, coordx: float, coordy: float) -> CCTVStrea
 def delete_cctv_stream(cctvname: str) -> CCTVStream:
     return cctv_stream_repo.delete(cctvname)
 
-@app.get("/task/record", tags=["cctv record task"])
-def read_cctv_record_list() -> list[TaskItem]:
-    return cctv_record_srv.get_tasks()
-
-@app.get("/task/record/start", tags=["cctv record task"])
-def start_cctv_record(cctv: str, startat: datetime, endat: datetime) -> TaskItem:
-    return cctv_record_srv.start(cctv=cctv, startat=startat, endat=endat)
-    
-@app.get("/task/record/stop/{taskid}", tags=["cctv record task"])
-def stop_cctv_record(taskid: str):
-    return cctv_record_srv.stop(taskid)
-
-@app.delete("/task/record/{taskid}", tags=["cctv record task"])
-def delete_cctv_record(taskid: str):
-    return cctv_record_srv.del_task(taskid)
+app.include_router(create_task_router(cctv_record_srv), prefix="/task/record")
+app.include_router(create_task_router(cctv_tracking_srv), prefix="/task/tracking")
 
 @app.get("/output", tags=["task output"])
 def read_task_output_list() -> list[TaskOutput]:
